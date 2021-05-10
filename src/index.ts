fc5d4ec88a0983a068eac62f9375e6d15f6a871c
Josh Liu <yunzheli@usc.edu>
2021-05-09 23:51:35 -0700

    set up interfaces, route class, install mongoose, .env

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ import * as expressWinston from "express-winston";
 import cors from "cors";
 import debug from "debug";
 
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
 const port = 3000;
